69045c8aabde39c9d3b47285d97a19af8d328028
Stanislav <adm.job@yandex.ru>
2026-03-05 13:25:11 +0500

    Update Route.cs

--- Assets/Scripts/Route.cs
+++ Assets/Scripts/Route.cs
@@ -2,21 +2,24 @@ using UnityEngine;
 
 public class Route : MonoBehaviour
 {
-    [SerializeField] private Transform[] _pointArray;
     [SerializeField] private float _speed;
-    [SerializeField] private Transform _pointParent;
+    [SerializeField] private Transform _way;
 
+    private Transform[] _pointArray;
     private int _index = 0;
-    
+
+    #if UNITY_EDITOR
+    [ContextMenu("Refresh Child Array")]
     private void Start()
     {
-        _pointArray = new Transform[_pointParent.childCount];
+        _pointArray = new Transform[_way.childCount];
 
-        for (int i = 0; i < _pointParent.childCount; i++)
+        for (int i = 0; i < _way.childCount; i++)
         {
-            _pointArray[i] = _pointParent.GetChild(i).GetComponent<Transform>();
+            _pointArray[i] = _way.GetChild(i);
         }
     }
+    #endif
 
     private void Update()
     {
@@ -25,7 +28,7 @@ public class Route : MonoBehaviour
 
         transform.position = Vector3.MoveTowards(transform.position, _pointRoute.position, _speed * Time.deltaTime);
 
-        if  ((_pointRoute.position - transform.position).sqrMagnitude < distance * distance)
+        if ((_pointRoute.position - transform.position).sqrMagnitude < distance * distance)
         {
             NextPoint();
         }
